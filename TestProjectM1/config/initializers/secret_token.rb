765420a1c5a561b7579c1833f36b1c1909a28e93
Yuriy Melnychuk <melnychuk_ua@mail.ru>
2014-04-24 16:39:44 +0300

    Initialize repository

---
+++ TestProjectM1/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TestProjectM1::Application.config.secret_token = '967fcfff8210dad48734e98e07d0e69a731a519947547e947eef58f8cfef6c607b2b46a41d46952c698746780184fbc7f2f461697d00a725d810002804f9300d'
